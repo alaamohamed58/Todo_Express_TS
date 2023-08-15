3e68743ba9864f88417557bc81fd565572eea5f5
alaamohamed58 <83567605+alaamohamed58@users.noreply.github.com>
2023-08-15 17:54:26 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -90,7 +90,7 @@ const checkTodoDate = async (): Promise<Response | void> => {
 };
 
 const job = new CronJob(
-  "45 17 * * *", // Cron expression: Runs at 1:38 PM every day
+  "15 18 * * *", // Cron expression: Runs at 1:38 PM every day
   function () {
     checkTodoDate();
     console.log("Email send at 4:18 PM");
